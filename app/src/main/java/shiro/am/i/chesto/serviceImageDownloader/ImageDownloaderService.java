89611fc50ff3e0698b6dc1d802d836f8516ffd22
Subaru "Shiro" Tashiro <wightwulf1944@gmail.com>
2016-11-17 18:57:31 +0800

    WIP DownloadService

--- app/src/main/java/shiro/am/i/chesto/serviceImageDownloader/ImageDownloaderService.java
+++ app/src/main/java/shiro/am/i/chesto/serviceImageDownloader/ImageDownloaderService.java
@@ -2,14 +2,18 @@ package shiro.am.i.chesto.serviceImageDownloader;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 
 import com.bumptech.glide.Glide;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 
 import shiro.am.i.chesto.PostStore;
 import shiro.am.i.chesto.retrofitDanbooru.Post;
@@ -42,36 +46,43 @@ public final class ImageDownloaderService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         final Post post = PostStore.get(intent.getIntExtra("default", -1));
-        final File file = getImageFile(post.getId() + ".png");
-        boolean isSuccessful = false;
 
         try {
-            isSuccessful = Glide.with(this)
-                    .load(post.getLargeFileUrl())
-                    .asBitmap()
-                    .into(post.getImageWidth(), post.getImageHeight())
-                    .get()
-                    .compress(Bitmap.CompressFormat.PNG, 0, new FileOutputStream(file));
+            File sourceFile = getSourceFile(post);
+            File targetFile = getTargetFile(post);
+            copy(sourceFile, targetFile);
+            sendBroadcast(new Intent(
+                    Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(targetFile)
+            ));
         } catch (Exception e) {
             notificationHelper.notifyFailed();
             Timber.e(e, "Download error: %s", post.getLargeFileUrl());
         }
 
-        if (isSuccessful) {
-            sendBroadcast(new Intent(
-                    Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(file)
-            ));
-        }
-
         notificationHelper.notifyFinished();
     }
 
-    private static File getImageFile(String fileName) {
+    private File getSourceFile(Post post) throws Exception {
+        return Glide.with(this)
+                .load(post.getLargeFileUrl())
+                .downloadOnly(post.getImageWidth(), post.getImageHeight())
+                .get();
+    }
+
+    private static File getTargetFile(Post post) {
         final File picturesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         final File saveDir = new File(picturesDir, "Chesto");
         if (!saveDir.mkdirs()) {
-            Timber.d("getImageFile: saveDir not created");
+            Timber.d("getTargetFile: saveDir not created");
         }
-        return new File(saveDir, fileName);
+        return new File(saveDir, post.getId() + ".png");
+    }
+
+    private static void copy(File src, File dst) throws IOException {
+        FileChannel inChannel = new FileInputStream(src).getChannel();
+        FileChannel outChannel = new FileOutputStream(dst).getChannel();
+        inChannel.transferTo(0, inChannel.size(), outChannel);
+        inChannel.close();
+        outChannel.close();
     }
 }
