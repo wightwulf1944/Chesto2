780f7e0f2af42e7b78c3b1d3aadec7b0d7d3316a
Subaru "Shiro" Tashiro <wightwulf1944@gmail.com>
2016-11-24 17:26:55 +0800

    Reimplemented PagerAdapter view recycling

--- app/src/main/java/shiro/am/i/chesto/activityPost/PostPagerAdapter.java
+++ app/src/main/java/shiro/am/i/chesto/activityPost/PostPagerAdapter.java
@@ -1,6 +1,5 @@
 package shiro.am.i.chesto.activityPost;
 
-import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -16,6 +15,7 @@ import jp.wasabeef.glide.transformations.BlurTransformation;
 import shiro.am.i.chesto.PostStore;
 import shiro.am.i.chesto.R;
 import shiro.am.i.chesto.retrofitDanbooru.Post;
+import timber.log.Timber;
 import uk.co.senab.photoview.PhotoView;
 
 /**
@@ -24,37 +24,101 @@ import uk.co.senab.photoview.PhotoView;
 final class PostPagerAdapter extends PagerAdapter {
 
     private final AppCompatActivity mParent;
+    private final Queue<ViewHolder> recycleQueue;
+    private final Queue<ViewHolder> loadNewQueue;
+    //    private final Queue<ViewHolder> setPrimaryQueue;
+    private final Queue<PhotoView> recycledPhotoViews;
+    private int currentPrimaryPosition;
 
     PostPagerAdapter(AppCompatActivity parent) {
         mParent = parent;
+        recycleQueue = new LinkedList<>();
+        loadNewQueue = new LinkedList<>();
+//        setPrimaryQueue = new LinkedList<>();
+        recycledPhotoViews = new LinkedList<>();
+        recycledPhotoViews.add(new PhotoView(parent));
+        recycledPhotoViews.add(new PhotoView(parent));
+        recycledPhotoViews.add(new PhotoView(parent));
+        currentPrimaryPosition = -1;
     }
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        final PhotoView photoView = PhotoViewRecycler.getView(mParent, container);
-        final Post post = PostStore.get(position);
-
-        Glide.with(mParent)
-                .load(post.getLargeFileUrl())
-                .placeholder(R.drawable.ic_image_placeholder)
-                .error(R.drawable.ic_image_placeholder)
-                .thumbnail(
-                        Glide.with(mParent)
-                                .load(post.getSmallFileUrl())
-                                .bitmapTransform(new BlurTransformation(mParent, 1))
-                                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
-                )
-                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
-                .into(photoView);
-
-        return photoView;
+        ViewHolder vh = new ViewHolder(position);
+        loadNewQueue.add(vh);
+        return vh;
     }
+
+//    @Override
+//    public void setPrimaryItem(ViewGroup container, int position, Object object) {
+//        if (position != currentPrimaryPosition) {
+//            ViewHolder vh = ((ViewHolder) object);
+//            setPrimaryQueue.add(vh);
+//            currentPrimaryPosition = position;
+//        }
+//    }
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-        final PhotoView photoView = (PhotoView) object;
-        Glide.clear(photoView);
-        PhotoViewRecycler.recycleView(photoView, container);
+        ViewHolder vh = ((ViewHolder) object);
+        recycleQueue.add(vh);
+    }
+
+    @Override
+    public void finishUpdate(ViewGroup container) {
+        while (!recycleQueue.isEmpty()) {
+            final ViewHolder vh = recycleQueue.remove();
+            Timber.d("%s Recycled", vh.position);
+            recycledPhotoViews.add(vh.photoView);
+            container.removeView(vh.photoView);
+
+            Glide.clear(vh.photoView);
+        }
+
+        while (!loadNewQueue.isEmpty()) {
+            final ViewHolder vh = loadNewQueue.remove();
+            Timber.d("%s Loaded", vh.position);
+            vh.photoView = recycledPhotoViews.remove();
+            container.addView(vh.photoView);
+
+//            Glide.with(mParent)
+//                    .load(vh.post.getSmallFileUrl())
+//                    .bitmapTransform(new BlurTransformation(mParent, 1))
+//                    .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+//                    .into(vh.photoView);
+            Glide.with(mParent)
+                    .load(vh.post.getLargeFileUrl())
+                    .placeholder(R.drawable.ic_image_placeholder)
+                    .error(R.drawable.ic_image_broken)
+                    .thumbnail(
+                            Glide.with(mParent)
+                                    .load(vh.post.getSmallFileUrl())
+                                    .bitmapTransform(new BlurTransformation(mParent, 1))
+                                    .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                    )
+                    .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                    .into(vh.photoView);
+        }
+
+//        while (!setPrimaryQueue.isEmpty()) {
+//            final ViewHolder vh = setPrimaryQueue.remove();
+//            Timber.d("%s Set primary", vh.position);
+//
+//            Glide.with(mParent)
+//                    .load(vh.post.getLargeFileUrl())
+//                    .placeholder(R.drawable.ic_image_placeholder)
+//                    .error(R.drawable.ic_image_broken)
+//                    .thumbnail(
+//                            Glide.with(mParent)
+//                                    .load(vh.post.getSmallFileUrl())
+//                                    .bitmapTransform(new BlurTransformation(mParent, 1))
+//                                    .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+//                    )
+//                    .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+//                    .into(vh.photoView);
+//        }
+
+        Timber.d("Update finished");
     }
 
     @Override
@@ -64,29 +128,17 @@ final class PostPagerAdapter extends PagerAdapter {
 
     @Override
     public boolean isViewFromObject(View view, Object object) {
-        return view.equals(object);
+        return ((ViewHolder) object).photoView.equals(view);
     }
 
-    // Handles preserving and recycling views for this adapter
-    private static final class PhotoViewRecycler {
-        private static final Queue<PhotoView> recycledViews = new LinkedList<>();
+    private static final class ViewHolder {
+        private final int position;
+        private final Post post;
+        private PhotoView photoView;
 
-        private static PhotoView getView(Context context, ViewGroup container) {
-            final PhotoView view;
-
-            if (recycledViews.isEmpty()) {
-                view = new PhotoView(context);
-            } else {
-                view = recycledViews.remove();
-            }
-
-            container.addView(view);
-            return view;
-        }
-
-        private static void recycleView(PhotoView view, ViewGroup container) {
-            recycledViews.add(view);
-            container.removeView(view);
+        private ViewHolder(int position) {
+            this.position = position;
+            post = PostStore.get(position);
         }
     }
 }
