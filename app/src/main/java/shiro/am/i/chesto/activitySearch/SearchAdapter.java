bc4386f1123e3d31e4b3b14de7643d5fe1c4cf35
Subaru "Shiro" Tashiro <wightwulf1944@gmail.com>
2017-03-22 14:29:34 +0800

    SearchAdapter WIP

--- app/src/main/java/shiro/am/i/chesto/activitySearch/SearchAdapter.java
+++ app/src/main/java/shiro/am/i/chesto/activitySearch/SearchAdapter.java
@@ -1,34 +1,35 @@
 package shiro.am.i.chesto.activitySearch;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.util.SortedList;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.Collections;
-import java.util.List;
-
 import shiro.am.i.chesto.R;
 import shiro.am.i.chesto.retrofitDanbooru.Tag;
 
 /**
- * Created by Shiro on 7/29/2016.
+ * Created by Shiro on 3/20/2017.
  */
-final class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder> {
+final class SearchAdapter extends Adapter<ViewHolder> {
+
+    private final int HEADER = 0;
+    private final int TAG = 1;
 
     private final LayoutInflater inflater;
-    private List<Tag> data;
     private OnItemClickListener onItemClickListener;
+    private SortedList<Tag> items;
 
-    SearchAdapter(Context context) {
-        inflater = LayoutInflater.from(context);
-        data = Collections.emptyList();
+    SearchAdapter(Context c) {
+        inflater = LayoutInflater.from(c);
     }
 
-    void setData(List<Tag> l) {
-        data = l;
+    void setData(SortedList<Tag> data) {
+        items = data;
     }
 
     void setOnItemClickListener(OnItemClickListener l) {
@@ -36,28 +37,77 @@ final class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder>
     }
 
     @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Object data = items.get(position);
+
+        if (data instanceof String) {
+            return HEADER;
+        } else if (data instanceof Tag) {
+            return TAG;
+        }
+
+        throw new RuntimeException("Could not figure out item view type at position: " + position);
+    }
+
+    @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = inflater.inflate(R.layout.item_tag_searchsuggestion, parent, false);
-        return new ViewHolder(view);
+
+        if (viewType == HEADER) {
+            View view = inflater.inflate(R.layout.search_item_header, parent, false);
+            return new HeaderViewHolder(view);
+
+        } else if (viewType == TAG) {
+            View view = inflater.inflate(R.layout.search_item_tag, parent, false);
+            return new TagViewHolder(view);
+
+        }
+        throw new RuntimeException("Unexpected view type received: " + viewType);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        final Tag tag = data.get(position);
-        holder.postCount.setText(tag.getPostCountStr());
-        holder.name.setText(tag.getName());
+        int viewType = holder.getItemViewType();
+        Object item = items.get(position);
+
+        if (viewType == HEADER) {
+            HeaderViewHolder headerViewHolder = (HeaderViewHolder) holder;
+            String labelStr = (String) item;
+            headerViewHolder.label.setText(labelStr);
+
+        } else if (viewType == TAG) {
+            TagViewHolder tagViewHolder = (TagViewHolder) holder;
+            Tag tag = (Tag) item;
+            tagViewHolder.postCount.setText(tag.getPostCountStr());
+            tagViewHolder.name.setText(tag.getName());
+
+        }
     }
 
-    @Override
-    public int getItemCount() {
-        return data.size();
+    interface OnItemClickListener {
+        void onItemClick(String itemName);
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
+    private static final class HeaderViewHolder extends ViewHolder {
+
+        private final TextView label;
+
+        HeaderViewHolder(View v) {
+            super(v);
+            label = (TextView) v;
+        }
+    }
+
+    private final class TagViewHolder extends ViewHolder {
+
         private final TextView postCount;
         private final TextView name;
 
-        private ViewHolder(View v) {
+        TagViewHolder(View v) {
             super(v);
             postCount = (TextView) v.findViewById(R.id.postCount);
             name = (TextView) v.findViewById(R.id.name);
@@ -65,12 +115,8 @@ final class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder>
         }
 
         private void onClick() {
-            String itemName = data.get(getAdapterPosition()).getName();
+            String itemName = name.getText().toString();
             onItemClickListener.onItemClick(itemName);
         }
-    }
-
-    interface OnItemClickListener {
-        void onItemClick(String itemName);
     }
 }
