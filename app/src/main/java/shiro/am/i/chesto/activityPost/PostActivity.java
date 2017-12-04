b09235521a356da5a47bb2d4548b74efa08dd25b
Subaru "Shiro" Tashiro <wightwulf1944@gmail.com>
2017-12-04 10:31:53 +0800

    cleanup
    asdasd

--- app/src/main/java/shiro/am/i/chesto/activityPost/PostActivity.java
+++ app/src/main/java/shiro/am/i/chesto/activityPost/PostActivity.java
@@ -1,6 +1,5 @@
 package shiro.am.i.chesto.activitypost;
 
-import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -9,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.PagerSnapHelper;
 import android.support.v7.widget.RecyclerView;
@@ -21,15 +21,19 @@ import com.google.android.flexbox.FlexboxLayout;
 
 import shiro.am.i.chesto.R;
 import shiro.am.i.chesto.model.AlbumStack;
-import shiro.am.i.chesto.model.Post;
 import shiro.am.i.chesto.serviceimagedownloader.DownloadService;
 import shiro.am.i.chesto.subscription.Subscription;
 import shiro.am.i.chesto.viewmodel.PostAlbum;
+
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 /**
  * Created by Shiro on 8/18/2016.
  */
 public final class PostActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 0;
 
     private RecyclerView recyclerView;
     private BottomSheetBehavior bottomSheetBehavior;
@@ -118,38 +122,44 @@ public final class PostActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // TODO: grantResults may be empty
         if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            Post post = album.get(currentIndex);
-            DownloadService.queue(this, post);
-            Snackbar.make(recyclerView, "Download queued", Snackbar.LENGTH_SHORT).show();
+            invokeDownload();
         } else {
             Snackbar.make(recyclerView, "Please allow access to save image", Snackbar.LENGTH_SHORT).show();
         }
     }
 
     private boolean onMenuItemClicked(MenuItem item) {
-        Uri uri;
-        Intent intent;
         switch (item.getItemId()) {
             case R.id.action_download:
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+                invokeDownload();
                 return true;
             case R.id.action_open_browser:
-                uri = album.get(currentIndex).getWebUri();
-                intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
+                invokeOpenInBrowser();
                 return true;
             case R.id.action_share:
-                String url = album.get(currentIndex).getWebUrl();
-                uri = album.get(currentIndex).getWebUri();
-                intent = new Intent(Intent.ACTION_SEND, uri);
-                intent.putExtra(Intent.EXTRA_TEXT, url);
-                intent.setType("text/plain");
-                startActivity(Intent.createChooser(intent, "Share link - " + url));
+                invokeShare();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void invokeDownload() {
+        if (ContextCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE) == PERMISSION_GRANTED) {
+            DownloadService.queue(this, album.get(currentIndex));
+        } else {
+            String[] permissionStr = {WRITE_EXTERNAL_STORAGE};
+            ActivityCompat.requestPermissions(this, permissionStr, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private void invokeOpenInBrowser() {
+        String webUrl = album.get(currentIndex).getWebUrl();
+        Uri webUri = Uri.parse(webUrl);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webUri);
+        startActivity(intent);
     }
 
     private void finishAndReturnResult() {
@@ -157,6 +167,15 @@ public final class PostActivity extends AppCompatActivity {
         resultIntent.putExtra("default", currentIndex);
         setResult(RESULT_OK, resultIntent);
         finish();
+    }
+
+    private void invokeShare() {
+        String webUrl = album.get(currentIndex).getWebUrl();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, webUrl);
+        intent.setType("text/plain");
+        Intent chooserIntent = Intent.createChooser(intent, "Share link - " + webUrl);
+        startActivity(chooserIntent);
     }
 
     public void onInfoButtonClicked(View view) {
